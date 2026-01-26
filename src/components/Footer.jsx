beb789ccdb9354415cfded1cb212db1179cae37f
Okoosi Emmanuel <145022337+emm-ok@users.noreply.github.com>
2026-01-26 16:36:10 +0100

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='mx-8 py-12 px-4 relative shadow-md rounded-full mt-12 pt-8 flex flex-wrap justify-center items-center'>
+    <footer className='px-4 pt-12 mt-12'>
         <p className='textsm text-muted-foreground font-medium'>
             &copy;   {new Date().getFullYear()} Manuel, All rights reserved 
         </p>
